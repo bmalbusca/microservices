bfb507a8cbf0235a792103722d12d274a071e0bf
Bruno Figueiredo <brunocfigueiredo@tecnico.ulisboa.pt>
2019-10-01 13:51:31 +0100

    More simpler solution

--- lab1/remoteCalculator.py
+++ lab1/remoteCalculator.py
@@ -3,7 +3,7 @@ from calculator import *
 
 calc = rpnCalculator()
 
-s = socket()
+s = socket(AF_INET, SOCK_STREAM)
 host = '127.0.0.1'
 port = 12345
 
@@ -11,16 +11,18 @@ s.bind((host,port))
 s.listen(1)
 
 
+
+c, addr = s.accept()
+#print("Server connected to"+ str(addr))
+
 while True:
-    c, addr = s.accept()
+    byte = c.recv(1024)
     
-    while True:
-        byte = c.recv(1024)
+    data = byte.decode()
+    number = float(data)
+    calc.pushValue(number)
     
-        data = byte.decode()
-        number = float(data)
-        calc.pushValue(number)
+    print(calc.memory)
     
-        print(calc.memory)
-    
-    c.close()
+
+c.close()
