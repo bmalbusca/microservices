c4866fa5324dfb34bc787525177f8ec497da6af1
Bruno Figueiredo <brunocfigueiredo@tecnico.ulisboa.pt>
2019-10-02 11:36:34 +0100

    lab1 - final

--- lab1/calculator.py
+++ lab1/calculator.py
@@ -12,7 +12,18 @@ class  rpnCalculator:
     
     def popValue(self):
         return  self.memory.pop()
-    def add():
-            pass
-
+    def add(self):
+        val1 =0;
+        val2 =0;
+        if (self.memory) :
+            val1 =  self.memory.pop()
+            
+            try:
+                val2  = self.memory.pop()
+            except IndexError as e:
+                return val1            
+            return val1+val2
+        
+        else:
+            return None  
 
--- lab1/serverCalculator.py
+++ lab1/serverCalculator.py
@@ -1,14 +1,19 @@
 #!/usr/local/bin/python3
 # which python3
 
-import pickle 
+import pickle
+import os 
 from  socket import *
 from calculator import *
 
-try:
-    dump_in = open("dump.txt", 'rb')
-    calc = pickle.load(dump_in)
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+my_file = os.path.join(THIS_FOLDER, 'myfile.txt')
 
+try:
+    dump_in = open(my_file, 'rb')
+    calc = pickle.load(dump_in)
+    dump_in.close()
+    print("Server - Loading data...abs")
 except:
     calc = rpnCalculator()
 
@@ -19,32 +24,45 @@ port = 12345
 s.bind((host,port))
 s.listen(1)
 
-
-
 c, addr = s.accept()
 print("Server connected to"+ str(addr))
-
 counter = 0;
+
 while True:
-    
+
     try:
-       
+
         byte = c.recv(1024)
  
         data = byte.decode()
-        number = float(data)
-        calc.pushValue(number)
-    
-        response = str(calc.popValue())
-        c.send(response.encode())
+        
+        string = data.split(" ")
+
+        if string[0] == "push":
+            number = float(string[1])
+            calc.pushValue(number)
+            response = "ACKN"
+            c.send(response.encode())
+        elif string[0] == "pop":
+            response = str(calc.popValue())
+            c.send(response.encode())
+        elif string[0] == "add":
+            response = str(calc.add())
+            c.send(response.encode())
+        else:
+            response = "NOP"
+            c.send(response.encode())
+
         print("Server - Received, Acknowledge and Reply %i" %counter)
         counter +=1;
    
     except KeyboardInterrupt:
-        print("W: interrupt received, saving data...")
-        dump_out = open("dump.txt ","wb")
+        print("Server - Ctrl-C interrupt received, saving data...")
+        dump_out = open(my_file,'wb')
         pickle.dump(calc ,dump_out)
         dump_out.close();
+        s.close()
+        c.close()
         break
     
 
