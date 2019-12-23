f2c3afbb53ceb329cf00f22d80862850147b5d42
Bruno Figueiredo <brunocfigueiredo@tecnico.ulisboa.pt>
2019-12-23 17:08:20 +0000

    All can seen via Internet

--- API_project/src/backend/api.py
+++ API_project/src/backend/api.py
@@ -7,7 +7,7 @@ from datetime import  *
 app = Flask(__name__)
 
 host = "0.0.0.0"
-#host = "localhost"
+public_ip = req.get('https://api.ipify.org').text
 http = "http://"
 port = "5000"
 
@@ -20,9 +20,9 @@ temporary_log = []
 
 
 web_pages = {
-        "room":http +  host + ":" + port + "/room" ,
-        "canteen":http +  host + ":" + port  + "/menu" ,
-        "secretariat":http +  host + ":" + port + "/secretariat"
+        "room":http +  str(public_ip) + ":" + port + "/room" ,
+        "canteen":http +  str(public_ip) + ":" + port  + "/menu" ,
+        "secretariat":http + str(public_ip) + ":" + port + "/secretariat"
         }
 
 
@@ -33,13 +33,12 @@ hash_services = {
         }
 
 proxy = {
-        "room": "http://0.0.0.0:5001/room/" ,
-        "canteen": "http://0.0.0.0:5003/menu/" ,
-        "secretariat": "http://0.0.0.0:5002/secretariat/"
+        "room": "http://"+str(public_ip)+":5001/room/" ,
+        "canteen": "http://"+str(public_ip)+":5003/menu/" ,
+        "secretariat": "http://"+str(public_ip)+":5002/secretariat/"
         }
 
       
-
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -85,22 +84,67 @@ def api_hashtable(subpath):
 @app.route('/', methods= ['GET', 'POST'])
 def index(proxies={}):
     global web_pages
-    web_pages["canteen"] = url_for('menu')
-    
+       
     proxies = web_pages 
     temporary_log.append(logs.message('/index/'))
     print("LOG:", len(temporary_log), temporary_log)
     return render_template("index.html", proxies=proxies)
 
+
+
+
+
+
+
 @app.route('/menu/search', methods= ['POST'])
-def search():
-    
+def menu_search():
+    print("proxy", proxy["canteen"])
+    print("menu - POST ")
     if  request.method == 'POST':
         day = request.form['day']
         m = request.form['m']
         y = request.form['y']
         add = proxy["canteen"] + day + "/" + m + "/" + y
         data= req.get(add).text
+        print(add)
+        temporary_log.append(logs.message(add))
+        return json2html.convert(json = data)
+    
+    else:
+
+        return redirect(url_for('menu'))
+
+
+
+
+@app.route('/room/search', methods= ['POST'])
+def room_search():
+    
+    if  request.method == 'POST':
+        num = request.form['id']
+        add = proxy["room"] + str(num)
+        print(add)
+        data= req.get(add).text
+        temporary_log.append(logs.message(add))
+        return json2html.convert(json = data)
+    
+    else:
+
+        return redirect(url_for('menu'))
+
+
+
+
+
+@app.route('/secretariat/search', methods= ['POST'])
+def set_search():
+    print("entrou") 
+    if  request.method == 'POST':
+        name = request.form['name']
+        local = request.form['local']
+        add = proxy["secretariat"] + str(name) + "/" + str(local) 
+        data= req.get(add).text
+        print(add)
         temporary_log.append(logs.message(add))
         return json2html.convert(json = data)
     
@@ -113,8 +157,24 @@ def search():
 @app.route('/menu/', methods= ['GET', 'POST'])
 def menu():
     temporary_log.append(logs.message('/menu/'))
+    print(web_pages["canteen"])
     return render_template("menu.html", ref = web_pages["canteen"]+"/search" )
      
+
+@app.route('/room/', methods = ['GET','POST'])
+def room():
+    temporary_log.append(logs.message('/room/'))
+    return render_template("room.html", ref=web_pages["room"]+"/search")
+
+
+
+
+@app.route('/secretariat/', methods = ['GET','POST'])
+def set():
+    temporary_log.append(logs.message('/secretariat/'))
+    return render_template("set.html", ref=web_pages["secretariat"]+"/search")
+
+
 
 
 if __name__ == '__main__':
